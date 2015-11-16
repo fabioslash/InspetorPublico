aa6c1fa354c01bb5e13a78c7264ee4ed73eec4bc
Fabio Lima <fabioslash1@gmail.com>
2015-11-16 17:32:51 -0200

    Spring data JPA

---
+++ src/main/java/br/unifor/pin5/service/InitDbService.java
@@ -0,0 +1,53 @@
+package br.unifor.pin5.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.unifor.pin5.entity.Role;
+import br.unifor.pin5.entity.User;
+import br.unifor.pin5.repository.RoleRepository;
+import br.unifor.pin5.repository.UserRepository;
+
+@Transactional
+@Service
+public class InitDbService {
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	
+	@PostConstruct
+	public void init() {
+		
+		Role roleUser = new Role();
+		roleUser.setName("ROLE_USER");
+		roleRepository.save(roleUser);
+		
+		Role roleAdmin = new Role();
+		roleAdmin.setName("ROLE_ADMIN");
+		roleRepository.save(roleAdmin);
+		
+		User userAdmin = new User();
+		//userAdmin.setEnabled(true);
+		userAdmin.setName("admin");
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		userAdmin.setPassword(encoder.encode("slash"));
+		List<Role> roles = new ArrayList<Role>();
+		roles.add(roleAdmin);
+		roles.add(roleUser);
+		userAdmin.setRoles(roles);
+		userRepository.save(userAdmin);
+		
+	
+	}
+}
